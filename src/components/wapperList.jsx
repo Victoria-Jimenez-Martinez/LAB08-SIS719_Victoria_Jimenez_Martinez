cbcda808180519717ca7c251ddaa0829d528bfcd
Jimenez-Martinez-Victoria <edenvictoriajimenes@gmail.com>
2024-04-20 06:37:54 -0600

    characteristic (common): [lab08-File-evaluation-purchase-of-the-week]

---
+++ src/components/wapperList.jsx
@@ -0,0 +1,14 @@
+import "../layouts/Style/wapperList.css"; 
+
+export const List = (props) => {
+    const { children } = props
+    return (
+      <div>
+        <h2>Compras de la Semana</h2>
+        <div>
+          { children }
+        </div>
+      </div>
+    )
+  }
+
